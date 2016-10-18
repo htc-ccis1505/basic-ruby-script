412b07c0803a03a4855479c29688170e4dcbb3d5
Mary Mosman <mbMosman@users.noreply.github.com>
2016-10-18 08:03:50 -0500

    Create lab1.rb

---
+++ lab1.rb
@@ -0,0 +1,13 @@
+##############################################################################
+# 
+# Update this script to:
+#
+# - Ask for the person's name
+# - Greet the player by name
+# - Ask for the person's age (in years)
+# - Tell the person how old they are in minutes (based on the years entered)
+# - Ask the person what the temperature is in degrees Fahrenheit
+# - Tell the person what the temperature is in degrees Celsius
+# - Thank the person by name for stopping by
+#
+#############################################################################
